996564b155f26ebc9a0eefb0efdb1f05f39c2033
Ryota Kakui <ryota6363@gmail.com>
2022-10-18 11:42:45 +0900

    Removed useeffect

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { Link as Scroll } from "react-scroll";
 import Link from "next/link";
 import Head from "next/head";
--- pages/works/navbar-works.js
+++ pages/works/navbar-works.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import Link from "next/link";
 import Head from "next/head";
 
